f39edc22387b6513b01ea39c1321d5de37260582
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-12-15 02:24:06 -0300

    Inializing app: React, Router and AntDesign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/login/login";
+import Dashboard from "./components/dashboard/dashboard";
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Switch>        
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
---
+++ src/components/login/login.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Row, Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+   } from "react-router-dom";
+import "./login.css";
+const Login = () => {
+  return (
+    <Row justify="center" className="loginForm" >
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: false,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: false,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Recordarme</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Olvide mi password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+           <Link to="/dashboard">Login</Link>
+          </Button>
+          
+        </Form.Item>
+      </Form>
+    </Row>
+  );
+};
+
+export default Login;
